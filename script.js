e90644755e6f24751758b6dba227372de4bc8643
jillcrawford <jrcrawford@anderson.edu>
2025-11-17 12:40:34 -0500

    Get DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// DOM elements
+// income
+const incomeForm = document.getElementById("income-form")
+const incomeAmount = document.getElementById("income-amount")
+const incomeList = document.getElementById("income-list")
+
+// expenses
+const expenseForm = document.getElementById("expense-form")
+const expenseDescription = document.getElementById("expense-description")
+const expenseAmount = document.getElementById("expense-amount")
+const expenseList = document.getElementById("expense-list")
+
+// totals
+const totalIncome = document.getElementById("total-income")
+const totalExpenses = document.getElementById("total-expenses")
+const remainingBalance = document.getElementById("remaining-balance")
 
